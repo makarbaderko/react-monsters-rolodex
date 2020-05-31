78ef86c1b016febe016e10270725d2487310d3c0
Makar Baderko <makarbaderko@gmail.com>
2020-05-31 21:13:24 +0300

    Added names from fake server, placed them in a grid.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
+import {CardList} from './components/card-list/card-list.component'
 import './App.css';
+
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-      string: 'Hello Makar'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Drakula',
+          id: 'asc2'
+        },
+        {
+          name : 'Zombie',
+          id: 'asc3'
+        }
+      ]
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters : users}))
   }
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.state.string}</p>
-        <button onClick= {() => this.setState({string: "Hello U!"})}>
-          Change Text
-        </button>
-      </header>
+    <CardList name='Makar'>{
+        this.state.monsters.map(monster => <h1>{monster.name}</h1>)
+      }</CardList>
     </div>
   )
   };
