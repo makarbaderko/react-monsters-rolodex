140fff523215a0013eceeb7afb8c169ab5f65ec7
Makar Baderko <makarbaderko@gmail.com>
2020-05-31 18:25:08 +0300

    added btn

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,20 +2,22 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello Makar'
+    }
+  }
   render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>My name is Makar.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button onClick= {() => this.setState({string: "Hello U!"})}>
+          Change Text
+        </button>
       </header>
     </div>
   )
