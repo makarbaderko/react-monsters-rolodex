61f974f903882875264961a5a3f84f3de97158b1
Makar Baderko <makarbaderko@gmail.com>
2020-06-01 12:06:03 +0300

    Updates

---
+++ precache-manifest.fb0ed5acd30af32c11b294bcb979f16a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e4212a8c9994cf6556334a9b340511",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80600a5378b63450cf5a",
+    "url": "/react-monsters-rolodex/static/css/main.72297a4c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80600a5378b63450cf5a",
+    "url": "/react-monsters-rolodex/static/js/main.a4f2414e.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
